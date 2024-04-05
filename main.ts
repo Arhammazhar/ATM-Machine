68f13f5172686387192d83f8c4e0961640608a9e
Arhammazhar <arhamatiq2007@gmail.com>
2024-04-04 21:52:00 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,47 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let myBalance = 5000;
+let myPin = 12345;
+
+console.log("WELCOME TO ATM MACHINE!!!");
+
+let pinAnswer = await inquirer.prompt([
+  {
+    name: "pin",
+    type: "number",
+    message: "Please Enter Your Pin Code:",
+  },
+]);
+if (pinAnswer.pin === myPin) {
+  console.log("Pin is correct login successful!");
+
+  let operationAnswer = await inquirer.prompt([
+    {
+      name: "operation",
+      type: "list",
+      choices: ["Withdraw Amount", "Check Balance"],
+    },
+  ]);
+  if (operationAnswer.operation === "Withdraw Amount") {
+    let amountAnswer = await inquirer.prompt([
+      {
+        name: "amount",
+        type: "number",
+        message: "Please Enter Amount To Withdraw",
+      },
+    ]);
+    if (amountAnswer.amount > myBalance) {
+      console.log("Insufficient Balance.");
+    } else {
+      myBalance -= amountAnswer.amount;
+      console.log(`${amountAnswer.amount} withdrawal successful.`);
+      console.log(`Your remaining balance is ${myBalance}.`);
+    }
+  } else if (operationAnswer.operation === "Check Balance") {
+    console.log(`Your Account Balance is ${myBalance}`);
+  }
+} else {
+  console.log("Login Unsuccessful, Please Enter Correct Pin Code...");
+}
